65e6d6362af7394152d696d54f484f51bf9f09e1
androhalin <androhalin@gmail.com>
2024-04-29 12:52:03 -0700

    Init Project

---
+++ src/app/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Sparsh Temani",
+  description: "My personal site",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
